b920cffc60132af0210474186191e9471f1670e7
riksof-shehzad <shehzad.bilal@riksof.com>
2012-12-10 17:06:33 +0500

    Update app/index.php

--- app/index.php
+++ app/index.php
@@ -5,6 +5,8 @@
  * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
+ 
+ini_set('memory_limit', '512M');
 
 require 'webroot/index.php';
 
