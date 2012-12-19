b2d6f4ad622b245bf3b9244e76ea8765a1737260
Ozair Kafray <ozair.nazir@riksof.com>
2012-12-19 17:36:36 +0500

    Added comments to Objects::count

--- app/models/Objects.php
+++ app/models/Objects.php
@@ -4,13 +4,21 @@ namespace app\models;
 
 class Objects extends \lithium\data\Model {
 
+
+    /**
+	 * The `count` method allows you to count documents matching the criteria specified in $conditions
+	 *
+	 * @param array $conditions The json search query as expected by MongoDB
+	 *
+	 * @return json object which only has a member count
+	 */
     public static function count($conditions) {
   
 	    $count = self::find('count', array('conditions' => $conditions));
 
         return array( 'count' => $count );
   
-  }
+    }
 
 }
 
